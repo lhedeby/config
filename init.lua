e60c96e75f40af4215549a0271439322642195c2
ludwig <ludwig@hedeby.me>
2023-10-23 18:42:15 +0200

    debbugings and windows

--- init.lua
+++ init.lua
@@ -27,6 +27,7 @@ require('lazy').setup({
   'tpope/vim-surround',
   'tpope/vim-sleuth',
   'mfussenegger/nvim-dap',
+  'rcarriga/nvim-dap-ui',
   'Issafalcon/lsp-overloads.nvim',
   { -- LSP Configuration & Plugins
     'neovim/nvim-lspconfig',
@@ -113,7 +114,7 @@ require('lazy').setup({
       }
     }
   },
-  {                  -- Adds git releated signs to the gutter, as well as utilities for managing changes
+  { -- Adds git releated signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
     opts = {
       -- See `:help gitsigns.txt`
@@ -133,7 +134,7 @@ require('lazy').setup({
       },
     },
   },
-  { "catppuccin/nvim", name = "catppuccin" },
+  { "catppuccin/nvim",               name = "catppuccin" },
   { -- Set lualine as statusline
     'nvim-lualine/lualine.nvim',
     -- See `:help lualine.txt`
@@ -203,6 +204,7 @@ vim.opt.relativenumber = true
 -- Enable mouse mode
 vim.o.mouse = 'a'
 vim.o.tabstop = 4
+vim.o.shiftwidth = 4
 vim.o.scrolloff = 10
 vim.cmd.colorscheme "catppuccin"
 
@@ -253,6 +255,7 @@ vim.o.termguicolors = true
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
 
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
@@ -307,6 +310,7 @@ vim.keymap.set('n', '<leader>ff', vim.lsp.buf.format, { desc = '[F]ormat Code' }
 vim.keymap.set('v', '<leader>ff', vim.lsp.buf.format, { desc = '[F]ormat Code' })
 
 vim.keymap.set('n', '<leader>fs', '<cmd>w<CR>', { desc = '[S]ave' })
+vim.keymap.set('n', '<leader>w', '<c-w>', { desc = '[W]indow' })
 
 
 
@@ -430,7 +434,7 @@ local on_attach = function(_, bufnr)
   nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
   nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
   nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-  nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+  -- nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
 
   -- See `:help K` for why this keymap
   nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
@@ -438,11 +442,11 @@ local on_attach = function(_, bufnr)
 
   -- Lesser used LSP functionality
   nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-  nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
-  nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
-  nmap('<leader>wl', function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, '[W]orkspace [L]ist Folders')
+  -- nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
+  -- nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
+  -- nmap('<leader>wl', function()
+  -- print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  -- end, '[W]orkspace [L]ist Folders')
 
   -- Create a command `:Format` local to the LSP buffer
   vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
@@ -509,11 +513,14 @@ mason_lspconfig.setup_handlers {
 }
 
 -- debug setup
-local dap = require "dap"
+local dap, dapui = require("dap"), require("dapui")
+
+
 
 dap.adapters.coreclr = {
   type = 'executable',
-  command = 'C:/Users/ludwi/Documents/netcoredbg/netcoredbg',
+  -- command = 'C:/Users/ludwi/AppData/Local/nvim-data/mason/bin/netcoredbg',
+  command = 'C:/Users/ludwi/Documents/netcoredbg/netcoredbg/netcoredbg.exe',
   args = { '--interpreter=vscode' }
 }
 dap.configurations.cs = {
@@ -526,6 +533,35 @@ dap.configurations.cs = {
     end,
   },
 }
+
+dapui.setup()
+
+vim.keymap.set('n', '<leader>do', function()
+  dapui.open()
+end, { desc = "[O]pen" })
+
+vim.keymap.set('n', '<leader>dp', function()
+  dapui.close()
+end, { desc = "[P]close" })
+
+dap.listeners.after.event_initialized["dapui_config"] = function()
+  dapui.open()
+end
+dap.listeners.before.event_terminated["dapui_config"] = function()
+  dapui.close()
+end
+dap.listeners.before.event_exited["dapui_config"] = function()
+  dapui.close()
+end
+
+
+
+vim.keymap.set('n', '<leader>dc', "<cmd>:lua require('dap').continue()<CR>")
+vim.keymap.set('n', '<leader>db', "<cmd>:lua require('dap').toggle_breakpoint()<CR>")
+
+vim.keymap.set('n', '<F10>', function() dap.step_over() end)
+
+
 -- nvim-cmp setup
 local cmp = require 'cmp'
 local luasnip = require 'luasnip'
