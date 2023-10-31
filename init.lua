e36aa673e4e9f4348cb123195f073bf1eb1385f0
ludwig <ludwig@hedeby.me>
2023-10-31 18:44:18 +0100

    cool functions for debugging

--- init.lua
+++ init.lua
@@ -515,6 +515,40 @@ mason_lspconfig.setup_handlers {
 -- debug setup
 local dap, dapui = require("dap"), require("dapui")
 
+vim.g.dotnet_build_project = function()
+  local default_path = vim.fn.getcwd() .. '/'
+  if vim.g['dotnet_last_proj_path'] ~= nil then
+    default_path = vim.g['dotnet_last_proj_path']
+  end
+  local path = vim.fn.input('Path to your *proj file', default_path, 'file')
+  vim.g['dotnet_last_proj_path'] = path
+  local cmd = 'dotnet build -c Debug ' .. path .. ' > /dev/null'
+  print('')
+  print('Cmd to execute: ' .. cmd)
+  local f = os.execute(cmd)
+  if f == 0 then
+    print('\nBuild: ✔️ ')
+  else
+    print('\nBuild: ❌ (code: ' .. f .. ')')
+  end
+end
+
+vim.g.dotnet_get_dll_path = function()
+  local request = function()
+    return vim.fn.input('Path to dll', vim.fn.getcwd() .. '/bin/Debug/', 'file')
+  end
+
+  if vim.g['dotnet_last_dll_path'] == nil then
+    vim.g['dotnet_last_dll_path'] = request()
+  else
+    if vim.fn.confirm('Do you want to change the path to dll?\n' .. vim.g['dotnet_last_dll_path'], '&yes\n&no', 2) == 1 then
+      vim.g['dotnet_last_dll_path'] = request()
+    end
+  end
+
+  return vim.g['dotnet_last_dll_path']
+end
+
 
 
 dap.adapters.coreclr = {
@@ -529,7 +563,11 @@ dap.configurations.cs = {
     name = "launch - netcoredbg",
     request = "launch",
     program = function()
-      return vim.fn.input('Path to dll', vim.fn.getcwd() .. '/bin/Debug/', 'file')
+      -- return vim.fn.input('Path to dll', vim.fn.getcwd() .. '/bin/Debug/', 'file')
+      if vim.fn.confirm('Should I recompile first?', '&yes\n&no', 2) == 1 then
+        vim.g.dotnet_build_project()
+      end
+      return vim.g.dotnet_get_dll_path()
     end,
   },
 }
@@ -559,11 +597,11 @@ end
 -- vim.keymap.set('n', '<leader>dc', "<cmd>:lua require('dap').continue()<CR>")
 -- vim.keymap.set('n', '<leader>db', "<cmd>:lua require('dap').toggle_breakpoint()<CR>")
 
-vim.keymap.set('n', '<leader>dc', function() dap.continue() end, { desc = "[C]ontinue/Start Debugging"})
-vim.keymap.set('n', '<leader>dq', function() dap.step_over() end, { desc = "[Q] Step over"})
-vim.keymap.set('n', '<leader>dw', function() dap.step_into() end, { desc = "[W] Step into"})
-vim.keymap.set('n', '<leader>de', function() dap.step_out() end, { desc = "[E] Step out"})
-vim.keymap.set('n', '<leader>db', function() dap.toggle_breakpoint() end, { desc = "Set [B]reakpoint"})
+vim.keymap.set('n', '<leader>dc', function() dap.continue() end, { desc = "[C]ontinue/Start Debugging" })
+vim.keymap.set('n', '<leader>dq', function() dap.step_over() end, { desc = "[Q] Step over" })
+vim.keymap.set('n', '<leader>dw', function() dap.step_into() end, { desc = "[W] Step into" })
+vim.keymap.set('n', '<leader>de', function() dap.step_out() end, { desc = "[E] Step out" })
+vim.keymap.set('n', '<leader>db', function() dap.toggle_breakpoint() end, { desc = "Set [B]reakpoint" })
 
 
 
